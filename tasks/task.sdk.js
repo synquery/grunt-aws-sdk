ee7538b02ea4750e31ab8053dd58ee9e24e47142
okubo <okubo@east-cloud.co.jp>
2013-11-19 12:53:16 +0900

    0.2.0

--- tasks/task.sdk.js
+++ tasks/task.sdk.js
@@ -2,36 +2,63 @@ var path = require('path');
 var AWS = require('aws-sdk');
 
 module.exports = function(grunt) {
+  var srv = grunt.file.readJSON(path.join(__dirname, 'aws.service.json'));
+  var getService = function(name, credentials) {
+    var info = srv[name];
+    AWS.config.update(credentials);
+    AWS.config.apiVersions[name] = info.apiVersion;
+    return new AWS[info.namespace];
+  };
 
-    var srv = grunt.file.readJSON(path.join(__dirname, 'aws.service.json'));
-    var getService = function(name, credentials) {
-        var info = srv[name];
-        AWS.config.update(credentials);
-        AWS.config.apiVersions[name] = info.apiVersion;
-        return new AWS[info.namespace];
-    };
+  var response = grunt.config.data.aws.data = {};
 
-    grunt.registerMultiTask('aws', function() {
-        var credentials = this.options().credentials || {};
-        var target = this.target, names = target.split('.');
+  grunt.registerMultiTask('aws', function() {
 
-        var name = names[0], fname = names[1];
-
-        if(!srv[name])
-            return grunt.warn('The service "' + name + '" is not supported.');
-
-        var service = getService(name, credentials), fn = service[fname];
-
-        if(typeof fn !== 'function')
-            return grunt.warn('No such function "' + fname + '" in ' + name
-                + ' service.');
-
-        var done = this.async();
-        fn.call(service, this.data || {}, function(err, res) {
-            if(err)
-                return done(err);
-            grunt.event.emit('ok', target, res);
-            done();
-        });
+    var target = this.target;
+    var data = this.data;
+    var options = this.options({
+      credentials: {},
+      dest: null
     });
+
+    var credentials = options.credentials;
+    var sname = data.service, name = sname;
+    var fname = data.method, params = data.params || {};
+
+    if(typeof name === 'string')
+      name = name.toLowerCase();
+
+    if(!srv[name])
+      return grunt.warn('The service "' + sname + '" is not supported.');
+
+    var service = getService(name, credentials), fn = service[fname];
+
+    if(typeof fn !== 'function')
+      return grunt.warn('No such function "' + fname + '" in ' + sname
+        + ' service.');
+
+    grunt.verbose.debug('Request:');
+    grunt.verbose.debug(JSON.stringify(params));
+
+    var fncs = name + '.' + fname + '()';
+    grunt.verbose.write('Connecting to AWS with ');
+    grunt.verbose.write(grunt.log.wordlist([fncs]) + '...');
+
+    var done = this.async();
+    fn.call(service, params || {}, function(err, res) {
+      if(err)
+        return done(err);
+
+      var str = JSON.stringify(res);
+
+      grunt.verbose.ok();
+      grunt.verbose.debug('Response:');
+      grunt.verbose.debug(str);
+
+      response[target] = res;
+      if(typeof options.dest === 'string')
+        grunt.file.write(path.join(options.dest, target), str);
+      done();
+    });
+  });
 };
